ba1c4efb1ef1c47f71fcc0b05fc2655aae5f9455
Kwabena Boakye <kwaboakye93@gmail.com>
2022-11-14 21:40:42 +0000

    Initial commit

---
+++ myScript.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+
+
+#ECHO COMMAND
+
+echo Hello, World!
+
+# VARIABLES
+# Uppercase by convention
+#
+# NAME = "Kobi"
+
+echo My name is $NAME
+
+read -p "Enter your name: " NAME
+
+echo $NAME nice to meet you
+
