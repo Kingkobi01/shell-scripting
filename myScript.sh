b23afa4e48657a4c2680f52a45f897c34eecee0f
Kwabena Boakye <kwaboakye93@gmail.com>
2022-11-14 22:24:03 +0000

    Another commit

--- myScript.sh
+++ myScript.sh
@@ -3,16 +3,40 @@
 
 #ECHO COMMAND
 
-echo Hello, World!
-
+# echo Hello, World!
 # VARIABLES
 # Uppercase by convention
 #
-# NAME = "Kobi"
 
-echo My name is $NAME
 
-read -p "Enter your name: " NAME
 
-echo $NAME nice to meet you
+#echo My name is $NAME
 
+# echo "Enter your name: "
+# read NAME
+
+# echo $NAME nice to meet you
+NAME="Kwabena"
+if [ "$NAME" = "Kwabena" ]
+then
+   echo "Nice One There!!"
+elif [ "$NAME" = "Kobi" ]
+then
+  echo "That's a nice name"
+else
+  echo "That's not also bad" 
+fi
+
+
+
+NUM1=12
+NUM2=21
+if [ "$NUM1" -gt "$NUM2" ]
+then
+  echo "$NUM1" is greater than "$NUM2"
+elif [ "$NUM1" -eq "$NUM2" ] 
+then
+  echo "$NUM1" is equal to "$NUM2"
+else
+  echo "$NUM1" is less than "$NUM2"
+fi
