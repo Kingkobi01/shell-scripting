56f6cfa7a87e217526249b349b74893b5b8d9de4
Kwabena Boakye <kwaboakye93@gmail.com>
2022-11-21 13:36:30 +0000

    Let's commit s'more

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 #! /usr/bin/bash
 
+echo "Bash location: $BASH"
+echo "Bash version: $BASH_VERSION"
+echo "Home Directory: $HOME"
+echo "Present working directory: $PWD"
+
+NAME = "Kwabena Boakye"
+echo = "My name is $NAME"
