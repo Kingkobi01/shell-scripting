9b2f140d93ccb8f035c64e8534259f2c062b301d
Kwabena Boakye <kwaboakye93@gmail.com>
2022-11-22 15:22:14 +0000

    Another commit

--- script.sh
+++ script.sh
@@ -1,6 +1,15 @@
 #! usr/bin/bash
 
-read -p "Enter name: " NAME
+read -p "WHat is your name: " NAME
+read -p "How old are you: " AGE
+AGELIM=18
 
-echo "Good Morning, $NAME"
+echo "Hi,"
 
+if [ "$AGE" -ge "$AGELIM" ]
+then
+    echo "Congratulations $NAME, you qualify for the license"
+else
+    DIF=$((18-$AGE))
+    echo "Sorry, $NAME, come back $DIF year(s) time"
+fi
